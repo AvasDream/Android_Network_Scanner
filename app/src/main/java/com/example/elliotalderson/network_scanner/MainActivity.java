132a01a5e9b8d6a67c75281b2f3aa55f959c9590
Vincent Grimmeisen <vigrimme@htwg-konstanz.de>
2018-05-02 20:38:08 +0200

    Added Ping functionality and separated worker thread.

--- app/src/main/java/com/example/elliotalderson/network_scanner/MainActivity.java
+++ app/src/main/java/com/example/elliotalderson/network_scanner/MainActivity.java
@@ -1,18 +1,21 @@
 package com.example.elliotalderson.network_scanner;
 
+import android.graphics.Color;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.Formatter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.Inet4Address;
-import java.net.InetAddress;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,10 +26,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+        String ownip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
         Button mButton = (Button)findViewById(R.id.scan_btn);
         final EditText mEdit   = (EditText)findViewById(R.id.ip_input);
         final TextView output = (TextView)findViewById(R.id.output_view);
+        final TextView ownIp = (TextView)findViewById(R.id.ownIp);
+        ownIp.setText("Device Address:\n" + ownip);
         mButton.setOnClickListener(
                 new View.OnClickListener()
                 {
@@ -36,27 +42,38 @@ public class MainActivity extends AppCompatActivity {
                         //List<Integer> openPorts = scan(ip,79,82);
                         System.out.println( mEdit.getText().toString());
                         String out = "";
-
-
-                        InetAddress IP = null;
-                        try {
-                            IP = InetAddress.getByName(ip);
-                            try {
-                                if (IP.isReachable(1)) {
-                                    output.setText("Host is up");
-                                } else {
-                                    output.setText("Host is down");
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        } catch (UnknownHostException e) {
-                            e.printStackTrace();
+                        pingTask p = new pingTask();
+                        Object[] objectArray = {ip};
+                        String ping = p.doInBackground(objectArray);
+                        System.out.printf("\n***\n***\n%s\n***\n***\n", ping);
+                        if (ping.contains("100% packet loss")) {
+                            output.setText(ip + " is offline!");
+                            output.setBackgroundColor(Color.RED);
+                        } else {
+                            output.setText(ip + " is online!");
+                            output.setBackgroundColor(Color.GREEN);
                         }
-
-
                     }
                 });
+    }
+    public String ping(String ip) {
+        try {
+        Process process = Runtime.getRuntime().exec(
+                "/system/bin/ping -c 3 " + ip);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(
+                process.getInputStream()));
+        int i;
+        char[] buffer = new char[4096];
+        StringBuffer output = new StringBuffer();
+        while ((i = reader.read(buffer)) > 0){
+            output.append(buffer, 0, i);
+        }
+        reader.close();
+        return output.toString();
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+        return "Error in Method ping()";
     }
 
     public List<Integer> scan(String ip, int port_beginn, int port_end) {
@@ -70,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return openPorts;
     }
+
     public Boolean isOpen(String ip, int port) {
         try {
             Socket socket = new Socket();
@@ -80,22 +98,28 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     }
+    private static class pingTask extends AsyncTask {
+        @Override
+        protected String doInBackground(Object[] objects) {
+            try {
+                String ip = objects[0].toString();
+                Process process = Runtime.getRuntime().exec(
+                        "/system/bin/ping -c 3 " + ip);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        process.getInputStream()));
+                int i;
+                char[] buffer = new char[4096];
+                StringBuffer output = new StringBuffer();
+                while ((i = reader.read(buffer)) > 0){
+                    output.append(buffer, 0, i);
+                }
+                reader.close();
+                return output.toString();
+            } catch (IOException e) {
+                return "Error in Method ping()";
+            }
 
+        }
+    }
 }
 
-                        /*
-
-                        InetAddress addr = null;
-                        try {
-                            addr = InetAddress.getByName(ip);
-                        } catch (UnknownHostException e) {
-                            e.printStackTrace();
-                        }
-                        if(!openPorts.isEmpty()) {
-                            for (int i = 0; i < openPorts.size(); i++) {
-                                out.concat("\n Port " + i + " is open");
-                            }
-                        } else {
-                            System.out.println("Why is this empty???");
-                        }
-                        */
